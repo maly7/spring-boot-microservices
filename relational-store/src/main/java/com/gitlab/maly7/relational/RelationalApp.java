112d898c272dcccc8e9d05b952297e720b838c65
maly7 <paul.aly@outlook.com>
2018-02-22 23:28:38 -0500

    #1 make first subproject boot runnable

---
+++ relational-store/src/main/java/com/gitlab/maly7/relational/RelationalApp.java
@@ -0,0 +1,11 @@
+package com.gitlab.maly7.relational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RelationalApp.class, args);
+    }
+}
