320d34114826031aedf1fde2b0ae23de3c8e3a6f
maly7 <paul.aly@outlook.com>
2018-02-25 22:26:28 -0500

    #2 test config

--- relational-store/src/test/java/com/gitlab/maly7/relational/ApplicationStartupTest.java
+++ relational-store/src/test/java/com/gitlab/maly7/relational/ApplicationStartupTest.java
@@ -3,10 +3,12 @@ package com.gitlab.maly7.relational;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(profiles = {"integration"})
 public class ApplicationStartupTest {
 
     @Test
