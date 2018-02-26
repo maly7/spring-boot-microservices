254d510eaee18a5aadc7794479f48e7b3fcbe344
maly7 <paul.aly@outlook.com>
2018-02-25 22:38:15 -0500

    #2 configure integTests

--- relational-store/src/integTest/java/com/gitlab/maly7/relational/ApplicationStartupTests.java
+++ relational-store/src/integTest/java/com/gitlab/maly7/relational/ApplicationStartupTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles(profiles = {"integration"})
-public class ApplicationStartupTest {
+public class ApplicationStartupTests {
 
     @Test
     public void contextLoads() {
