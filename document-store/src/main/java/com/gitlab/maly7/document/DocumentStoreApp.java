3fb205cd6f02d89b2993be51b1ef8264fe28cb19
maly7 <paul.aly@outlook.com>
2018-02-26 23:05:25 -0500

    #4 subpackaging

--- document-store/src/main/java/com/gitlab/maly7/document/DocumentStoreApp.java
+++ document-store/src/main/java/com/gitlab/maly7/document/DocumentStoreApp.java
@@ -1,4 +1,4 @@
-package com.gitlab.maly7;
+package com.gitlab.maly7.document;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
