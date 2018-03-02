443020c282b1c289f5f8870d5c45182ec5f3901f
maly7 <paul.aly@outlook.com>
2018-03-02 09:44:24 -0500

    make sure tests run on a random port

--- relational-store/src/integTest/java/com/gitlab/maly7/relational/ApplicationStartupTests.java
+++ relational-store/src/integTest/java/com/gitlab/maly7/relational/ApplicationStartupTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = {"integration"})
 public class ApplicationStartupTests {
 
