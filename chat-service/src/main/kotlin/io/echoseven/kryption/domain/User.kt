6b8455ebeb2b8c0b2766594d935716f27bf1ba81
Paul Aly <paul.aly@outlook.com>
2018-03-18 11:28:08 -0400

    #18 collections tests
    domain objects should be data classes

--- chat-service/src/main/kotlin/io/echoseven/kryption/domain/User.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/domain/User.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "users")
-class User(var email: String,
+data class User(var email: String,
            var name: String = "Tap to update your name",
            var onlineStatus: Boolean = false,
            var profileImageUrl: String = "noPhoto") {
