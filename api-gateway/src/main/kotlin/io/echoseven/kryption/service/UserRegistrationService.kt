5198e360008d0b4651443d30eaa45fd808f00e7a
maly7 <paul.aly@outlook.com>
2018-03-19 15:47:12 -0400

    #7 working on feign client

--- api-gateway/src/main/kotlin/io/echoseven/kryption/service/UserRegistrationService.kt
+++ api-gateway/src/main/kotlin/io/echoseven/kryption/service/UserRegistrationService.kt
@@ -1,9 +1,10 @@
 package io.echoseven.kryption.service
 
+import io.echoseven.kryption.clients.ChatServiceClient
 import io.echoseven.kryption.domain.UserSignup
 import org.springframework.stereotype.Service
 
 @Service
-class UserRegistrationService {
-    fun registerUser(userSignup: UserSignup) = userSignup
+class UserRegistrationService(private val chatServiceClient: ChatServiceClient) {
+    fun registerUser(userSignup: UserSignup) = chatServiceClient.createUser(userSignup)
 }
