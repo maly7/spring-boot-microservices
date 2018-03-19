59122fd3d304c6ba6847c0d27484efea66d7531b
maly7 <paul.aly@outlook.com>
2018-03-19 12:25:00 -0400

    #7 failing test for user creation

--- chat-service/src/main/kotlin/io/echoseven/kryption/domain/User.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/domain/User.kt
@@ -4,10 +4,10 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "users")
-data class User(var email: String,
-           var name: String = "Tap to update your name",
-           var onlineStatus: Boolean = false,
-           var profileImageUrl: String = "noPhoto") {
+data class User(var email: String = "placeholder-email",
+                var name: String = "Tap to update your name",
+                var onlineStatus: Boolean = false,
+                var profileImageUrl: String = "noPhoto") {
     @Id
     var id: String? = null
 }
