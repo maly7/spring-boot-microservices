26fcdb65157c90455880ae592981f8e151dbf3cd
maly7 <paul.aly@outlook.com>
2018-03-19 19:37:58 -0400

    #7 happy path for registration

--- api-gateway/src/main/kotlin/io/echoseven/kryption/domain/UserSignup.kt
+++ api-gateway/src/main/kotlin/io/echoseven/kryption/domain/UserSignup.kt
@@ -2,4 +2,6 @@ package io.echoseven.kryption.domain
 
 import javax.validation.constraints.NotBlank
 
-data class UserSignup(@NotBlank var email: String? = null, @NotBlank var password: String? = null)
+data class UserSignup(var id: String? = null,
+                      @NotBlank var email: String? = null,
+                      @NotBlank var password: String? = null)
--- authentication-service/src/main/kotlin/io/echoseven/kryption/domain/UserAccount.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/domain/UserAccount.kt
@@ -1,10 +1,13 @@
 package io.echoseven.kryption.domain
 
-import org.hibernate.annotations.GenericGenerator
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
-import javax.persistence.*
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EntityListeners
+import javax.persistence.Id
 import javax.validation.constraints.NotBlank
 
 @Entity
@@ -14,9 +17,7 @@ data class UserAccount(@NotBlank @Column(name = "email", nullable = false) var e
                        @Column(name = "verified") var isVerified: Boolean = false) {
     @Id
     @Column(name = "id")
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    var id: String? = null
+    var id: String? = UUID.randomUUID().toString()
 
     @CreatedDate
     @Column(name = "created_date", nullable = false)
