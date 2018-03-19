78db68333064bd8c440a18344ba916df54b1ddbe
maly7 <paul.aly@outlook.com>
2018-03-19 19:13:40 -0400

    #7 ChatServiceClient -> ChatClient

--- api-gateway/src/main/kotlin/io/echoseven/kryption/service/UserRegistrationService.kt
+++ api-gateway/src/main/kotlin/io/echoseven/kryption/service/UserRegistrationService.kt
@@ -1,10 +1,10 @@
 package io.echoseven.kryption.service
 
-import io.echoseven.kryption.clients.ChatServiceClient
+import io.echoseven.kryption.clients.ChatClient
 import io.echoseven.kryption.domain.UserSignup
 import org.springframework.stereotype.Service
 
 @Service
-class UserRegistrationService(private val chatServiceClient: ChatServiceClient) {
-    fun registerUser(userSignup: UserSignup) = chatServiceClient.createUser(userSignup)
+class UserRegistrationService(private val chatClient: ChatClient) {
+    fun registerUser(userSignup: UserSignup) = chatClient.createUser(userSignup)
 }
