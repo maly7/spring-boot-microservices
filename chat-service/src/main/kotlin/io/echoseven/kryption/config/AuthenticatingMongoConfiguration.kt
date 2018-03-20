067c5bbc92ba28ab2ca1ef4ae1fdfb8a9a8129f3
maly7 <paul.aly@outlook.com>
2018-03-19 20:14:03 -0400

    #7 limit the mongo config to not integration

--- chat-service/src/main/kotlin/io/echoseven/kryption/config/AuthenticatingMongoConfiguration.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/config/AuthenticatingMongoConfiguration.kt
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.mongo.MongoProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 
 @Configuration
+@Profile("!integration")
 class AuthenticatingMongoConfiguration : AbstractMongoConfiguration() {
 
     @Autowired
