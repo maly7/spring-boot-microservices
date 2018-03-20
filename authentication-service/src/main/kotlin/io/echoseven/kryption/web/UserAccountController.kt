efad75aece5f994c6cb372891bcec4a763e62899
maly7 <paul.aly@outlook.com>
2018-03-20 19:52:59 -0400

    #7 password encoding with BCrypt

--- authentication-service/src/main/kotlin/io/echoseven/kryption/web/UserAccountController.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/web/UserAccountController.kt
@@ -1,7 +1,7 @@
 package io.echoseven.kryption.web
 
-import io.echoseven.kryption.data.UserAccountRepository
 import io.echoseven.kryption.domain.UserAccount
+import io.echoseven.kryption.service.UserAccountService
 import io.echoseven.kryption.web.resource.UserAccountResource
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
-class UserAccountController(private val userAccountRepository: UserAccountRepository) {
+class UserAccountController(private val userAccountService: UserAccountService) {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = ["/user"], consumes = [APPLICATION_JSON_UTF8_VALUE], produces = [APPLICATION_JSON_UTF8_VALUE])
     fun createUser(@Valid @RequestBody userAccount: UserAccount): UserAccountResource =
-            UserAccountResource(userAccountRepository.save(userAccount))
+            userAccountService.createUser(userAccount)
 
     // TODO: remove with #19
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/user")
-    fun deleteAllUsers(): Unit = userAccountRepository.deleteAll()
+    fun deleteAllUsers(): Unit = userAccountService.deleteAllUsers()
 
 }
