2a2acdad7b7989c9b518b9f1aa8c470726011af6
maly7 <paul.aly@outlook.com>
2018-03-19 20:49:16 -0400

    #7 embrace logging

--- chat-service/src/main/kotlin/io/echoseven/kryption/web/advice/DuplicateKeyAdvice.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/web/advice/DuplicateKeyAdvice.kt
@@ -1,5 +1,6 @@
 package io.echoseven.kryption.web.advice
 
+import org.slf4j.LoggerFactory
 import org.springframework.dao.DuplicateKeyException
 import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -8,9 +9,11 @@ import javax.servlet.http.HttpServletResponse
 
 @ControllerAdvice
 class DuplicateKeyAdvice {
+    private val log = LoggerFactory.getLogger(DuplicateKeyAdvice::class.java)
 
     @ExceptionHandler(value = [DuplicateKeyException::class])
     fun handleDuplicateKeyException(ex: DuplicateKeyException, response: HttpServletResponse) {
+        log.error("Duplicate Key Exception occurred, probably due to a duplicate email during signup")
         response.sendError(BAD_REQUEST.value(), "Duplicate data provided")
     }
 }
