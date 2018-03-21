8657a1ada5a33020a149184e1644d0f4f3df43d5
maly7 <paul.aly@outlook.com>
2018-03-21 17:15:31 -0400

    #16 test config

--- authentication-service/src/main/kotlin/io/echoseven/kryption/tokens/TokenIssuer.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/tokens/TokenIssuer.kt
@@ -1,29 +1,15 @@
 package io.echoseven.kryption.tokens
 
-import io.echoseven.kryption.config.TokenSettings
 import io.echoseven.kryption.domain.UserAccount
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
-import org.apache.commons.io.IOUtils
-import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
 import java.security.Key
-import java.security.KeyFactory
-import java.security.spec.PKCS8EncodedKeySpec
-import javax.annotation.PostConstruct
 
 @Component
-class TokenIssuer(private val tokenSettings: TokenSettings) {
-    private val signatureAlgorithm = SignatureAlgorithm.RS256
-    lateinit var signingKey: Key
-
-    @PostConstruct
-    fun init() {
-        val bytes = IOUtils.toByteArray(ClassPathResource("keys/private_key.der").inputStream)
-        val keySpec = PKCS8EncodedKeySpec(bytes)
-        signingKey = KeyFactory.getInstance(signatureAlgorithm.familyName).generatePrivate(keySpec)
-    }
+class TokenIssuer(private val signingKey: Key,
+                  private val signatureAlgorithm: SignatureAlgorithm) {
 
     fun issueToken(userAccount: UserAccount): String {
         return Jwts.builder()
