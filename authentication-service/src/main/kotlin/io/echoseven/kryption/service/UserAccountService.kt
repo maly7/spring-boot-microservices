182a81349b08ba3015f0d7a14a7d9dd0ed522e1e
maly7 <paul.aly@outlook.com>
2018-03-21 16:47:57 -0400

    #16 use rsa to make things better

--- authentication-service/src/main/kotlin/io/echoseven/kryption/service/UserAccountService.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/service/UserAccountService.kt
@@ -2,20 +2,27 @@ package io.echoseven.kryption.service
 
 import io.echoseven.kryption.data.UserAccountRepository
 import io.echoseven.kryption.domain.UserAccount
-import io.echoseven.kryption.web.resource.UserAccountResource
+import io.echoseven.kryption.tokens.TokenIssuer
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class UserAccountService(private val userAccountRepository: UserAccountRepository,
-                         private val passwordEncoder: PasswordEncoder) {
+                         private val passwordEncoder: PasswordEncoder,
+                         private val tokenIssuer: TokenIssuer) {
 
-    fun create(user: UserAccount): UserAccountResource {
+    fun create(user: UserAccount): UserAccount {
         user.password = passwordEncoder.encode(user.password)
-        return UserAccountResource(userAccountRepository.save(user))
+        return userAccountRepository.save(user)
     }
 
     fun get(email: String) = userAccountRepository.findByEmail(email)
+
+    fun getFromToken(token: String): Optional<UserAccount> {
+        val id = tokenIssuer.getIdFromToken(token)
+        return userAccountRepository.findById(id)
+    }
 
     // TODO: remove with #19
     fun deleteAllUsers() = userAccountRepository.deleteAll()
--- authentication-service/src/main/kotlin/io/echoseven/kryption/tokens/TokenIssuer.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/tokens/TokenIssuer.kt
@@ -5,15 +5,31 @@ import io.echoseven.kryption.domain.UserAccount
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.apache.commons.io.IOUtils
+import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
+import java.security.Key
+import java.security.KeyFactory
+import java.security.spec.PKCS8EncodedKeySpec
+import javax.annotation.PostConstruct
 
 @Component
 class TokenIssuer(private val tokenSettings: TokenSettings) {
+    private val signatureAlgorithm = SignatureAlgorithm.RS256
+    lateinit var signingKey: Key
+
+    @PostConstruct
+    fun init() {
+        val bytes = IOUtils.toByteArray(ClassPathResource("keys/private_key.der").inputStream)
+        val keySpec = PKCS8EncodedKeySpec(bytes)
+        signingKey = KeyFactory.getInstance(signatureAlgorithm.familyName).generatePrivate(keySpec)
+    }
+
     fun issueToken(userAccount: UserAccount): String {
         return Jwts.builder()
                 .setSubject(userAccount.email)
                 .setId(userAccount.id)
-                .signWith(SignatureAlgorithm.HS512, tokenSettings.secret)
+                .signWith(signatureAlgorithm, signingKey)
                 .compact()
     }
 
@@ -23,7 +39,7 @@ class TokenIssuer(private val tokenSettings: TokenSettings) {
 
     private fun parseClaims(token: String): Claims {
         return Jwts.parser()
-                .setSigningKey(tokenSettings.secret)
+                .setSigningKey(signingKey)
                 .parseClaimsJws(token)
                 .body
     }
