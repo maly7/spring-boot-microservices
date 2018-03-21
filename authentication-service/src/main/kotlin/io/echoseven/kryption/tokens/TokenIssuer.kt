06ce69af5bdca8d8db85fa127d016c404cd1e228
maly7 <paul.aly@outlook.com>
2018-03-21 14:39:17 -0400

    #16 successful user login

--- authentication-service/src/main/kotlin/io/echoseven/kryption/tokens/TokenIssuer.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/tokens/TokenIssuer.kt
@@ -2,6 +2,7 @@ package io.echoseven.kryption.tokens
 
 import io.echoseven.kryption.config.TokenSettings
 import io.echoseven.kryption.domain.UserAccount
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.stereotype.Component
@@ -12,7 +13,18 @@ class TokenIssuer(private val tokenSettings: TokenSettings) {
         return Jwts.builder()
                 .setSubject(userAccount.email)
                 .setId(userAccount.id)
-                .signWith(SignatureAlgorithm.RS512, tokenSettings.secret)
+                .signWith(SignatureAlgorithm.HS512, tokenSettings.secret)
                 .compact()
+    }
+
+    fun getEmailFromToken(token: String): String {
+        return parseClaims(token).subject
+    }
+
+    private fun parseClaims(token: String): Claims {
+        return Jwts.parser()
+                .setSigningKey(tokenSettings.secret)
+                .parseClaimsJws(token)
+                .body
     }
 }
