240ac0dfc4f22a39d3e26295ef66b47efe8a175b
maly7 <paul.aly@outlook.com>
2018-03-21 13:53:33 -0400

    #16 embracing logging

--- authentication-service/src/main/kotlin/io/echoseven/kryption/service/AuthenticationService.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/service/AuthenticationService.kt
@@ -4,6 +4,7 @@ import io.echoseven.kryption.domain.UserAccount
 import io.echoseven.kryption.exception.UnauthorizedException
 import io.echoseven.kryption.exception.UserNotFoundException
 import io.echoseven.kryption.tokens.TokenIssuer
+import org.slf4j.LoggerFactory
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
@@ -11,17 +12,23 @@ import org.springframework.stereotype.Service
 class AuthenticationService(private val userAccountService: UserAccountService,
                             private val tokenIssuer: TokenIssuer,
                             private val passwordEncoder: PasswordEncoder) {
+    private val log = LoggerFactory.getLogger(AuthenticationService::class.java)
 
     fun authenticate(authRequest: UserAccount): String {
-        val userAccountOptional = userAccountService.get(authRequest.email!!)
+        val requestEmail = authRequest.email!!
+        log.debug("Received login request for user email: [{}]", requestEmail)
+
+        val userAccountOptional = userAccountService.get(requestEmail)
 
         if (!userAccountOptional.isPresent) {
-            throw UserNotFoundException("No user found with email: " + authRequest.email)
+            log.warn("Failed login attempt for email [{}] because the user was not found", requestEmail)
+            throw UserNotFoundException("No user found with email: $requestEmail")
         }
 
         val existingUser = userAccountOptional.get()
 
         if (!passwordEncoder.matches(authRequest.password, existingUser.password)) {
+            log.debug("Failed login attempt for email [{}] due to bad password", requestEmail)
             throw UnauthorizedException("Password provided does not match")
         }
 
