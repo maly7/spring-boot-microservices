b701d7ec194320e4f43c133fe94a994d552ed50e
maly7 <paul.aly@outlook.com>
2018-03-27 21:13:17 -0400

    #23 working on auth filter

---
+++ chat-service/src/main/kotlin/io/echoseven/kryption/security/ChatUser.kt
@@ -0,0 +1,37 @@
+package io.echoseven.kryption.security
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+data class ChatUser(var token: String, var email: String, var userName: String) : Authentication, UserDetails {
+    private var authenticated: Boolean = true
+
+    override fun getUsername(): String = email
+
+    override fun getName(): String = userName
+
+    override fun getPrincipal(): Any = this
+
+    override fun getDetails(): Any = emptyMap<String, String>()
+
+    override fun getCredentials(): String = token
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAuthenticated(): Boolean = authenticated
+
+    override fun setAuthenticated(isAuthenticated: Boolean) {
+        this.authenticated = isAuthenticated
+    }
+
+    override fun getPassword(): String = ""
+}
