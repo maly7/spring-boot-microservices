f27dc8b8a2171428e238bd949f8f82a468037bf6
maly7 <paul.aly@outlook.com>
2018-03-28 21:01:49 -0400

    #23 first stab at auth filter for chat app

--- chat-service/src/main/kotlin/io/echoseven/kryption/ChatApp.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/ChatApp.kt
@@ -2,9 +2,13 @@ package io.echoseven.kryption
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 class ChatApp
 
--- chat-service/src/main/kotlin/io/echoseven/kryption/security/SecurityConfig.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/security/SecurityConfig.kt
@@ -3,6 +3,7 @@ package io.echoseven.kryption.security
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -21,6 +22,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeRequests().anyRequest().authenticated()
                 .antMatchers("/actuator/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/**").permitAll()
                 .and().addFilter(tokenAuthenticationFilter).anonymous().disable()
     }
 
