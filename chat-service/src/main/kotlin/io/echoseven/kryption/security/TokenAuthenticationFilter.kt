d28157d71d658820afdd8b757bb78a0e26d0de26
maly7 <paul.aly@outlook.com>
2018-04-06 16:20:23 -0400

    #23 reworking auth token stuff
    still need to redo filter

--- chat-service/src/main/kotlin/io/echoseven/kryption/security/TokenAuthenticationFilter.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/security/TokenAuthenticationFilter.kt
@@ -1,38 +1,36 @@
 package io.echoseven.kryption.security
 
 import io.echoseven.kryption.clients.AuthenticationClient
-import io.echoseven.kryption.data.UserRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter
 import javax.servlet.http.HttpServletRequest
 
 class TokenAuthenticationFilter : AbstractPreAuthenticatedProcessingFilter() {
+    private val log = LoggerFactory.getLogger(TokenAuthenticationFilter::class.java)
 
     @Autowired
     lateinit var authenticationClient: AuthenticationClient
 
-    @Autowired
-    lateinit var userRepository: UserRepository
+    override fun getPreAuthenticatedCredentials(request: HttpServletRequest?): Any = authenticate(request!!)
 
-    override fun getPreAuthenticatedCredentials(request: HttpServletRequest?): Any = authenticate(request!!).credentials
+    override fun getPreAuthenticatedPrincipal(request: HttpServletRequest?): Any = authenticate(request!!)
 
-    override fun getPreAuthenticatedPrincipal(request: HttpServletRequest?): Any = authenticate(request!!).principal
-
-    private fun authenticate(request: HttpServletRequest): ChatSecurityContextUser {
+    private fun authenticate(request: HttpServletRequest) {
         val token = request.getHeader(HttpHeaders.AUTHORIZATION)
                 ?: throw UsernameNotFoundException("No authorization token provided")
 
         val authUser = authenticationClient.authenticate(token)
-        val appUser = userRepository.findById(authUser.id).orElseThrow {
-            UsernameNotFoundException("No User found for provided token with id [${authUser.id}]")
-        }
 
-        val chatUser = ChatSecurityContextUser(token, appUser.email, appUser.email)
+        val auth = AuthenticationToken(mutableListOf(SimpleGrantedAuthority("Kryption-User")))
+        auth.token = token
+        auth.id = authUser.id
+        log.debug("Successfully filtered authentication for [{}]", auth.token)
 
-        SecurityContextHolder.getContext().authentication = chatUser
-        return chatUser
+        SecurityContextHolder.getContext().authentication = auth
     }
 }
