9d6f5eac5751c2cf711f93652c108136647aa6e9
Paul Aly <paul.aly@outlook.com>
2018-04-07 18:53:12 -0400

    #23 cleanup + more tests

--- chat-service/src/integTest/kotlin/io/echoseven/kryption/web/UserRestTests.kt
+++ chat-service/src/integTest/kotlin/io/echoseven/kryption/web/UserRestTests.kt
@@ -2,10 +2,11 @@ package io.echoseven.kryption.web
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule
 import io.echoseven.kryption.ChatIntegrationTest
-import io.echoseven.kryption.clients.AuthUser
 import io.echoseven.kryption.data.UserRepository
 import io.echoseven.kryption.domain.User
+import io.echoseven.kryption.support.createUser
 import io.echoseven.kryption.support.stubAuthUser
+import io.echoseven.kryption.support.tokenHeaders
 import org.junit.After
 import org.junit.Rule
 import org.junit.Test
@@ -13,13 +14,12 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
 @RunWith(SpringRunner::class)
@@ -84,20 +84,23 @@ class UserRestTests {
 
     @Test
     fun `Only an authenticated user should be able to request their own user info`() {
-        val userToCreate = User("email@foo.com")
         val token = "foo-user"
+        val originalUser = createUser(User("email@foo.com"), restTemplate)
+        stubAuthUser(wireMock, token, originalUser)
 
-        val originalUser: User = restTemplate.postForEntity("/user", userToCreate, User::class.java).body!!
-        stubAuthUser(wireMock, token, AuthUser(originalUser.id.toString(), originalUser.email, true))
-
-        val headers = HttpHeaders()
-        headers.accept = listOf(MediaType.APPLICATION_JSON_UTF8)
-        headers[HttpHeaders.AUTHORIZATION] = listOf(token)
-
-        val entity = HttpEntity("", headers)
-
+        val entity = HttpEntity("", tokenHeaders(token))
         val userLookupResponse: User = restTemplate.exchange("/user", HttpMethod.GET, entity, User::class.java).body!!
 
         assertEquals(originalUser, userLookupResponse, "The retrieved user should be the same as the created user")
+
+        val secondUser = createUser(User("second.user@foo.com"), restTemplate)
+        val secondToken = "other-user"
+        stubAuthUser(wireMock, secondToken, secondUser)
+
+        val secondEntity = HttpEntity("", tokenHeaders(secondToken))
+        val secondUserLookup = restTemplate.exchange("/user", HttpMethod.GET, secondEntity, User::class.java).body!!
+
+        assertEquals(secondUser, secondUserLookup, "The second user should match the second created user")
+        assertNotEquals(userLookupResponse, secondUserLookup, "The two fetched users should not match")
     }
 }
