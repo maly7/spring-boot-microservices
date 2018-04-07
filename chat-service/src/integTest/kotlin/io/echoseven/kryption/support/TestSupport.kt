93ab9bd0fd25ebe6ed77db444d59f832182a2be6
Paul Aly <paul.aly@outlook.com>
2018-04-07 18:53:12 -0400

    #23 cleanup + more tests

--- chat-service/src/integTest/kotlin/io/echoseven/kryption/support/TestSupport.kt
+++ chat-service/src/integTest/kotlin/io/echoseven/kryption/support/TestSupport.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import com.github.tomakehurst.wiremock.junit.WireMockRule
 import io.echoseven.kryption.clients.AuthUser
+import io.echoseven.kryption.domain.User
+import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -17,4 +19,18 @@ fun stubAuthUser(rule: WireMockRule, token: String, user: AuthUser) {
             .withStatus(HttpStatus.OK.value())
             .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .withBody(objectMapper.writeValueAsString(user))))
+}
+
+fun stubAuthUser(rule: WireMockRule, token: String, user: User) =
+    stubAuthUser(rule, token, AuthUser(user.id.toString(), user.email, true))
+
+fun tokenHeaders(token: String): HttpHeaders {
+    val headers = HttpHeaders()
+    headers.accept = listOf(MediaType.APPLICATION_JSON_UTF8)
+    headers[HttpHeaders.AUTHORIZATION] = listOf(token)
+    return headers
+}
+
+fun createUser(user: User, restTemplate: TestRestTemplate): User {
+    return restTemplate.postForEntity("/user", user, User::class.java).body!!
 }
