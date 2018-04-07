f51ee0a11b4c4e413a45e7b9d40442db3dac324e
Paul Aly <paul.aly@outlook.com>
2018-04-07 12:15:22 -0400

    #23 existing tests pass

--- chat-service/src/main/kotlin/io/echoseven/kryption/security/SecurityConfig.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/security/SecurityConfig.kt
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+    private final val userPattern: String = "/user/**"
+    private final val actuatorPattern: String = "/actuator/**"
 
     @Autowired
     lateinit var authenticationProvider: JwtAuthenticationProvider
@@ -23,10 +26,15 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().authorizeRequests()
-            .antMatchers("/actuator/**").permitAll()
-            .antMatchers("/user/**").permitAll()
             .anyRequest().authenticated()
             .and().addFilter(JwtAuthorizationFilter(authenticationManager(), authenticationClient)).anonymous().disable()
+    }
+
+    override fun configure(web: WebSecurity) {
+        web.ignoring()
+            .antMatchers(HttpMethod.POST, userPattern)
+            .antMatchers(HttpMethod.DELETE, userPattern)
+            .antMatchers(actuatorPattern)
     }
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
