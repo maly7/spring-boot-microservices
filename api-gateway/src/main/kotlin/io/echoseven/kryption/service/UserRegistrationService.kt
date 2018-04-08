e0f1ac5e21b08dcb9c6ccc69ad95130de81f83e7
Paul Aly <paul.aly@outlook.com>
2018-04-08 15:39:16 -0400

    #33 test passes, but need to handle creation failures better

--- api-gateway/src/main/kotlin/io/echoseven/kryption/service/UserRegistrationService.kt
+++ api-gateway/src/main/kotlin/io/echoseven/kryption/service/UserRegistrationService.kt
@@ -2,8 +2,8 @@ package io.echoseven.kryption.service
 
 import io.echoseven.kryption.clients.AuthenticationClient
 import io.echoseven.kryption.clients.ChatClient
-import io.echoseven.kryption.domain.UserResponse
 import io.echoseven.kryption.domain.UserRegistration
+import io.echoseven.kryption.domain.UserResponse
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -21,14 +21,8 @@ class UserRegistrationService(
 
         userRegistration.id = chatUser.id
 
-        try {
-            val authUser = authenticationClient.createUser(userRegistration)
-            log.debug("User [{}] successfully created in auth service", authUser)
-            return authUser
-        } catch (e: Exception) {
-            log.error("Error occurred during creation of user account [{}] in auth service, attempting to rollback chat service user", userRegistration.email)
-            chatClient.deleteUser(chatUser.id)
-            throw IllegalStateException("User Creation Failed")
-        }
+        val authUser = authenticationClient.createUser(userRegistration)
+        log.debug("User [{}] successfully created in auth service", authUser)
+        return authUser
     }
 }
