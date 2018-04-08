50912c380d94cbf130e65979957d760504cd6517
Paul Aly <paul.aly@outlook.com>
2018-04-08 11:59:43 -0400

    #33 send bad request status for multiple registration attempts

--- chat-service/src/main/kotlin/io/echoseven/kryption/web/advice/DuplicateKeyAdvice.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/web/advice/DuplicateKeyAdvice.kt
@@ -2,18 +2,21 @@ package io.echoseven.kryption.web.advice
 
 import org.slf4j.LoggerFactory
 import org.springframework.dao.DuplicateKeyException
-import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import javax.servlet.http.HttpServletResponse
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
-class DuplicateKeyAdvice {
+class DuplicateKeyAdvice : ResponseEntityExceptionHandler() {
     private val log = LoggerFactory.getLogger(DuplicateKeyAdvice::class.java)
 
     @ExceptionHandler(value = [DuplicateKeyException::class])
-    fun handleDuplicateKeyException(ex: DuplicateKeyException, response: HttpServletResponse) {
+    fun handleDuplicateKeyException(ex: DuplicateKeyException, request: WebRequest): ResponseEntity<Any> {
         log.error("Duplicate Key Exception occurred, probably due to a duplicate email during registration")
-        response.sendError(BAD_REQUEST.value(), "Duplicate data provided")
+        return handleExceptionInternal(ex, "Duplicate Entry", HttpHeaders(), HttpStatus.BAD_REQUEST, request)
     }
 }
