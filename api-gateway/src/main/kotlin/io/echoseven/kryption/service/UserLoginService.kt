7b2d59f9bed62b69488caf5deda306b1b371ec50
Paul Aly <paul.aly@outlook.com>
2018-04-08 21:09:57 -0400

    #33 removing direct zuul route to login
    tests pass now

--- api-gateway/src/main/kotlin/io/echoseven/kryption/service/UserLoginService.kt
+++ api-gateway/src/main/kotlin/io/echoseven/kryption/service/UserLoginService.kt
@@ -4,6 +4,7 @@ import io.echoseven.kryption.clients.AuthenticationClient
 import io.echoseven.kryption.clients.ChatClient
 import io.echoseven.kryption.domain.UserAuth
 import io.echoseven.kryption.domain.UserResponse
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,10 +12,14 @@ class UserLoginService(
     private val chatClient: ChatClient,
     private val authenticationClient: AuthenticationClient
 ) {
+    private val log = LoggerFactory.getLogger(UserLoginService::class.java)
 
     fun login(userAuth: UserAuth): UserResponse {
         val token = authenticationClient.login(userAuth).token
+        log.debug("Successfully authenticated user [{}] and received token [{}]", userAuth.email, token)
+
         val user = chatClient.getUser(token)
+        log.debug("Fetched User [{}] from chat service", user)
 
         user.token = token
         user.id = ""
