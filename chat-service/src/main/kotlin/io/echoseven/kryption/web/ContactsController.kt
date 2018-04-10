621cfddc004c6b46a331393cc837727f1c6ee716
Paul Aly <paul.aly@outlook.com>
2018-04-09 23:04:02 -0400

    #29 stub out contacts controller

---
+++ chat-service/src/main/kotlin/io/echoseven/kryption/web/ContactsController.kt
@@ -0,0 +1,14 @@
+package io.echoseven.kryption.web
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/user")
+class ContactsController {
+    // GET /contacts - return list of current users contacts
+    // PUT /contacts - update the entire list of contacts
+    // PUT /contact - add a single contact
+    // DELETE /contact - delete a single contact
+    // GET /search?q={email} - search for a contact to add
+}
