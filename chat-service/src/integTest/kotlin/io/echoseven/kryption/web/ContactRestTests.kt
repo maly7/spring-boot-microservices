eeb6d57083ac96584ea1a3d99b1652554d89b277
maly7 <paul.aly@outlook.com>
2018-04-22 22:10:59 -0400

    #29 stub out contacts tests

--- chat-service/src/integTest/kotlin/io/echoseven/kryption/web/ContactRestTests.kt
+++ chat-service/src/integTest/kotlin/io/echoseven/kryption/web/ContactRestTests.kt
@@ -1,10 +1,26 @@
 package io.echoseven.kryption.web
 
 import io.echoseven.kryption.ChatIntegrationTest
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @ChatIntegrationTest
 class ContactRestTests {
+
+    @Test
+    fun `A User Should be able to add a Contact`() {
+
+    }
+
+    @Test
+    fun `A User should be able to retrieve their Contacts`() {
+
+    }
+
+    @Test
+    fun `Attempting to add a non-existent user as a contact should fail`() {
+
+    }
 }
