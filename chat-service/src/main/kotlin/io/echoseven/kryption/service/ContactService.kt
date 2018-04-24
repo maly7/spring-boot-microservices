5328401cc22fb28afd872d98b913e23e2cf95bef
maly7 <paul.aly@outlook.com>
2018-04-23 22:39:36 -0400

    #29 logging

--- chat-service/src/main/kotlin/io/echoseven/kryption/service/ContactService.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/service/ContactService.kt
@@ -5,6 +5,7 @@ import io.echoseven.kryption.exception.BadRequestException
 import io.echoseven.kryption.web.resource.Contact
 import io.echoseven.kryption.web.resource.ContactRequest
 import org.apache.commons.lang3.StringUtils
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,6 +13,7 @@ class ContactService(
     val userRepository: UserRepository,
     val userService: UserService
 ) {
+    private val log = LoggerFactory.getLogger(ContactService::class.java)
 
     fun getContacts(): List<Contact> {
         return userService.getCurrentUser().contacts.map { it.asContact() }
@@ -19,6 +21,7 @@ class ContactService(
 
     fun addContact(contactRequest: ContactRequest): List<Contact> {
         guardAgainstBadRequests(contactRequest)
+        log.debug("Attempting to add user [{}] to contact list", contactRequest.email)
 
         val contact = userService.findByEmail(contactRequest.email!!)
         val currentUser = userService.getCurrentUser()
@@ -31,6 +34,7 @@ class ContactService(
 
     fun removeContact(contactRequest: ContactRequest) {
         guardAgainstBadRequests(contactRequest)
+        log.debug("Attempting to remove user [{}] from contact list", contactRequest.email)
 
         val contact = userService.findByEmail(contactRequest.email!!)
         val currentUser = userService.getCurrentUser()
