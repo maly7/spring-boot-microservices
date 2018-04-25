750d28ec44922341b1764e363ae4670a0290551a
maly7 <paul.aly@outlook.com>
2018-04-24 21:20:17 -0400

    #29 stubbing out the rest of the needed tests

---
+++ functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/ContactTests.kt
@@ -0,0 +1,11 @@
+package io.echoseven.kryption.functional
+
+import org.junit.Test
+
+class ContactTests {
+
+    @Test
+    fun `A User should be able to add another user as a contact`() {
+
+    }
+}
