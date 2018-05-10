12fc7df1e6bb4553dacd7f3adaf3061d00f8871e
maly7 <paul.aly@outlook.com>
2018-05-10 19:56:01 -0400

    #27 initial chat and chat message classes

---
+++ chat-service/src/main/kotlin/io/echoseven/kryption/domain/Chat.kt
@@ -0,0 +1,13 @@
+package io.echoseven.kryption.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.DBRef
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "chats")
+data class Chat(
+    @DBRef var messages: List<ChatMessage> = listOf()
+) {
+    @Id
+    var id: String? = null
+}
---
+++ chat-service/src/main/kotlin/io/echoseven/kryption/domain/ChatMessage.kt
@@ -0,0 +1,20 @@
+package io.echoseven.kryption.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.ZonedDateTime
+import javax.validation.constraints.NotBlank
+
+@Document(collection = "chats")
+data class ChatMessage(
+    @NotBlank var message: String,
+    var fromId: String,
+    var toId: String,
+    var timestamp: ZonedDateTime,
+    var imageHeight: Int?,
+    var imageWidth: Int?,
+    var imageUrl: String? = null
+) {
+    @Id
+    var id: String? = null
+}
