8edf1fdc30e54d94a9ff7afeb55b10251da17e55
maly7 <paul.aly@outlook.com>
2018-05-10 22:18:19 -0400

    #27 remove unused import

--- chat-service/src/main/kotlin/io/echoseven/kryption/domain/ChatMessage.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/domain/ChatMessage.kt
@@ -3,7 +3,6 @@ package io.echoseven.kryption.domain
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.io.Serializable
-import java.time.ZonedDateTime
 import java.util.Date
 import javax.validation.constraints.NotBlank
 
