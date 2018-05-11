ce6d64e35e3328d7417e5493de6300e88437cce0
maly7 <paul.aly@outlook.com>
2018-05-10 20:42:51 -0400

    #27 adding messages to chats

--- chat-service/src/integTest/kotlin/io/echoseven/kryption/data/ChatRepositoryTests.kt
+++ chat-service/src/integTest/kotlin/io/echoseven/kryption/data/ChatRepositoryTests.kt
@@ -2,11 +2,16 @@ package io.echoseven.kryption.data
 
 import io.echoseven.kryption.ChatIntegrationTest
 import io.echoseven.kryption.domain.Chat
+import io.echoseven.kryption.domain.ChatMessage
+import org.hamcrest.Matchers.hasItem
+import org.hamcrest.Matchers.hasSize
 import org.junit.After
+import org.junit.Assert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.junit4.SpringRunner
+import java.time.ZonedDateTime
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
@@ -18,8 +23,12 @@ class ChatRepositoryTests {
     @Autowired
     lateinit var chatRepository: ChatRepository
 
+    @Autowired
+    lateinit var chatMessageRepository: ChatMessageRepository
+
     @After
     fun cleanup() {
+        chatMessageRepository.deleteAll()
         chatRepository.deleteAll()
     }
 
@@ -34,5 +43,21 @@ class ChatRepositoryTests {
 
         val foundChat = foundOptional.get()
         assertEquals(chat.id, foundChat.id)
+    }
+
+    @Test
+    fun `add messages to a chat`() {
+        val chat = chatRepository.save(Chat())
+
+        val messages = listOf(
+            chatMessageRepository.save(ChatMessage("test message", "foo", "bar", ZonedDateTime.now())),
+            chatMessageRepository.save(ChatMessage("second message", "bar", "foo", ZonedDateTime.now()))
+        )
+        chat.messages = messages
+
+        val updatedChat = chatRepository.save(chat)
+
+        assertThat("There should be two messages", updatedChat.messages, hasSize(2))
+        messages.forEach { assertThat("The message should be in the chat", chat.messages, hasItem(it)) }
     }
 }
