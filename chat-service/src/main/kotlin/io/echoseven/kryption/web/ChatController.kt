91d5b04b6e34d269aebe2887e382ed9610c03283
maly7 <paul.aly@outlook.com>
2018-05-15 22:33:06 -0400

    #8 first take at delete for a chat

--- chat-service/src/main/kotlin/io/echoseven/kryption/web/ChatController.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/web/ChatController.kt
@@ -3,11 +3,14 @@ package io.echoseven.kryption.web
 import io.echoseven.kryption.domain.Chat
 import io.echoseven.kryption.domain.ChatMessage
 import io.echoseven.kryption.service.ChatService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -18,9 +21,12 @@ class ChatController(val chatService: ChatService) {
     @PostMapping("/message")
     fun sendMessage(@Valid @RequestBody chatMessage: ChatMessage) = chatService.sendMessage(chatMessage)
 
-    // GET /chat/id - protect with method level security
     @GetMapping("/{id}")
     fun getChat(@PathVariable id: String): Chat = chatService.get(id)
 
-    // DELETE /chat/id - protect with method level security
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteChat(@PathVariable id: String) = chatService.delete(id)
+
+    // DELETE /chat/message/id
 }
