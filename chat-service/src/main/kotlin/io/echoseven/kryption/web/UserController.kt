3de03def101165324efd1ba8327e193a5489d3fb
maly7 <paul.aly@outlook.com>
2018-05-24 22:33:04 -0400

    #48 move functionality into user service

--- chat-service/src/main/kotlin/io/echoseven/kryption/web/UserController.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/web/UserController.kt
@@ -1,16 +1,9 @@
 package io.echoseven.kryption.web
 
-import io.echoseven.kryption.data.UserRepository
 import io.echoseven.kryption.domain.User
-import io.echoseven.kryption.exception.NotFoundException
-import io.echoseven.kryption.security.AuthenticationToken
-import org.slf4j.LoggerFactory
-import org.springframework.amqp.core.AmqpAdmin
-import org.springframework.amqp.core.Binding
-import org.springframework.amqp.core.Queue
+import io.echoseven.kryption.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -23,35 +16,21 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("/user")
-class UserController(val userRepository: UserRepository, val amqpAdmin: AmqpAdmin) {
-    private val log = LoggerFactory.getLogger(UserController::class.java)
+class UserController(val userService: UserService) {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = [APPLICATION_JSON_UTF8_VALUE], produces = [APPLICATION_JSON_UTF8_VALUE])
-    fun createUser(@Valid @RequestBody user: User): User {
-        log.debug("Creating User [{}]", user)
-        val createdUser = userRepository.insert(user)
-        val queueId = "user.${createdUser.id}"
-        amqpAdmin.declareQueue(Queue(queueId, true))
-        amqpAdmin.declareBinding(Binding(queueId, Binding.DestinationType.QUEUE, "user.updates", queueId, mapOf()))
-        return createdUser
-    }
+    fun createUser(@Valid @RequestBody user: User): User = userService.create(user)
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    fun deleteUser(@PathVariable id: String) = userRepository.deleteById(id)
+    fun deleteUser(@PathVariable id: String) = userService.delete(id)
 
     // TODO: remove with #19
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping
-    fun deleteAllUsers(): Unit = userRepository.deleteAll()
+    fun deleteAllUsers(): Unit = userService.deleteAll()
 
     @GetMapping
-    fun getUser(): User {
-        val auth: AuthenticationToken = (SecurityContextHolder.getContext().authentication as AuthenticationToken?)!!
-        log.debug("Fetching user data for [{}]", auth)
-        return userRepository.findById(auth.id).orElseThrow {
-            NotFoundException("A User could not be found for the current user, this should never happen")
-        }
-    }
+    fun getUser(): User = userService.getCurrentUser()
 }
