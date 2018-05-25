b4b9ad00a9b3619bc0807934941ee2b444b3bff5
maly7 <paul.aly@outlook.com>
2018-05-24 21:34:45 -0400

    #48 make sure we have an exchange created

--- chat-service/src/main/kotlin/io/echoseven/kryption/config/ApplicationConfig.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/config/ApplicationConfig.kt
@@ -1,5 +1,9 @@
 package io.echoseven.kryption.config
 
+import io.echoseven.kryption.properties.MessagingProperties
+import org.springframework.amqp.core.AmqpAdmin
+import org.springframework.amqp.core.FanoutExchange
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,4 +15,11 @@ class ApplicationConfig {
     @Bean
     @LoadBalanced
     fun restTemplate(): RestTemplate = RestTemplate()
+
+    @Bean
+    fun exchangeCreator(amqpAdmin: AmqpAdmin, messagingProperties: MessagingProperties): InitializingBean =
+        InitializingBean {
+            amqpAdmin.declareExchange(FanoutExchange("user.updates"))
+            amqpAdmin.declareQueue()
+        }
 }
