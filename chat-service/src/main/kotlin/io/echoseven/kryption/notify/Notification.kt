3cd2c1c8676cb30ee5cd6efc49f2ebab991c6834
maly7 <paul.aly@outlook.com>
2018-05-27 21:59:44 -0400

    #48 adding Notification class

---
+++ chat-service/src/main/kotlin/io/echoseven/kryption/notify/Notification.kt
@@ -0,0 +1,8 @@
+package io.echoseven.kryption.notify
+
+data class Notification(
+    val status: NotificationStatus,
+    val conversationId: String,
+    val messageId: String?,
+    val content: Any?
+)
