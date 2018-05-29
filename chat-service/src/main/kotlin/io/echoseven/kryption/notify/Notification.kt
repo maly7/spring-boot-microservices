96841c66f3eac4fb36d6ebb852bb53a672c624a0
maly7 <paul.aly@outlook.com>
2018-05-29 19:12:23 -0400

    #48 mega test for new conversation messages

--- chat-service/src/main/kotlin/io/echoseven/kryption/notify/Notification.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/notify/Notification.kt
@@ -2,7 +2,7 @@ package io.echoseven.kryption.notify
 
 data class Notification(
     val status: NotificationStatus,
-    val conversationId: String,
+    val conversationId: String?,
     val messageId: String?,
     val content: Any?
 )
