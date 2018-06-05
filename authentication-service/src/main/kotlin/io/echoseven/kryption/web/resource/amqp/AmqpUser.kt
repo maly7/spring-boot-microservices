83aa814b0b2a9059dcbea239b06da9d8ecf4b638
maly7 <paul.aly@outlook.com>
2018-06-05 19:15:23 -0400

    #54 stubbing out amqp auth controller

--- authentication-service/src/main/kotlin/io/echoseven/kryption/web/resource/amqp/AmqpUser.kt
+++
@@ -1,3 +0,0 @@
-package io.echoseven.kryption.web.resource.amqp
-
-data class AmqpUser(val username: String = "", val password: String = "", val tags: List<String> = listOf())
--- authentication-service/src/main/kotlin/io/echoseven/kryption/web/resource/amqp/ResourceCheck.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/web/resource/amqp/ResourceCheck.kt
@@ -1,3 +1,29 @@
 package io.echoseven.kryption.web.resource.amqp
 
-class ResourceCheck(var resource: String, var name: String, var permission: String) : Check()
+class ResourceCheck(var resource: String, var name: String, var permission: String) : Check() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
+
+        other as ResourceCheck
+
+        if (resource != other.resource) return false
+        if (name != other.name) return false
+        if (permission != other.permission) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + resource.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + permission.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "ResourceCheck(resource='$resource', name='$name', permission='$permission', username= '$username', vhost='$vhost')"
+    }
+}
