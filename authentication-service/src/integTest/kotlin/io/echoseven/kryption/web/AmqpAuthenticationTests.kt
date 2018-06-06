cfc6c3edfc33b4a91833fc6fd6944c8ca7ca3d3b
maly7 <paul.aly@outlook.com>
2018-06-05 22:24:55 -0400

    #54 topic test

--- authentication-service/src/integTest/kotlin/io/echoseven/kryption/web/AmqpAuthenticationTests.kt
+++ authentication-service/src/integTest/kotlin/io/echoseven/kryption/web/AmqpAuthenticationTests.kt
@@ -5,6 +5,8 @@ import io.echoseven.kryption.domain.UserAccount
 import io.echoseven.kryption.web.resource.TokenResource
 import io.echoseven.kryption.web.resource.UserAccountResource
 import io.echoseven.kryption.web.resource.amqp.ALLOW_ACCESS
+import io.echoseven.kryption.web.resource.amqp.DENY_ACCESS
+import io.echoseven.kryption.web.resource.amqp.TopicCheck
 import io.echoseven.kryption.web.resource.amqp.VirtualHostCheck
 import org.junit.Before
 import org.junit.Test
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
+import java.util.UUID
 import kotlin.test.assertEquals
 
 @AuthIntegrationTest
@@ -33,7 +36,7 @@ class AmqpAuthenticationTests {
     }
 
     @Test
-    fun `The vhost should always non-blank usernames`() {
+    fun `Vhost requests should always non-blank usernames`() {
         val check = VirtualHostCheck()
         check.vhost = "/"
         check.username = email
@@ -41,5 +44,16 @@ class AmqpAuthenticationTests {
 
         assertEquals(HttpStatus.OK, response.statusCode, "The response should be 200 OK")
         assertEquals(ALLOW_ACCESS, response.body, "The user should be allowed access")
+    }
+
+    @Test
+    fun `Topic requests should always deny`() {
+        val check = TopicCheck("user.*")
+        check.vhost = "/"
+        check.username = "${UUID.randomUUID()}"
+
+        val response = restTemplate.postForEntity("/amqp/topic", check, String::class.java)
+        assertEquals(HttpStatus.OK, response.statusCode, "The response should be 200 OK")
+        assertEquals(DENY_ACCESS, response.body, "The user should be denies access")
     }
 }
