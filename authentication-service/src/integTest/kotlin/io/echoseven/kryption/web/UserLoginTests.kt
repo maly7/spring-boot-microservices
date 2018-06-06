8f2edb7fe5e96f5c888d92358c26db056f0a37f2
maly7 <paul.aly@outlook.com>
2018-06-05 22:11:54 -0400

    #54 formatting

--- authentication-service/src/integTest/kotlin/io/echoseven/kryption/web/UserLoginTests.kt
+++ authentication-service/src/integTest/kotlin/io/echoseven/kryption/web/UserLoginTests.kt
@@ -22,8 +22,8 @@ import org.springframework.test.context.junit4.SpringRunner
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-@RunWith(SpringRunner::class)
 @AuthIntegrationTest
+@RunWith(SpringRunner::class)
 class UserLoginTests {
 
     @Autowired
@@ -50,7 +50,8 @@ class UserLoginTests {
 
     @Test
     fun `Attempting to login as a non-existing user should fail`() {
-        val failedResponse = restTemplate.postForEntity("/login", UserAccount("bad-email", "does not matter"), String::class.java)
+        val failedResponse =
+            restTemplate.postForEntity("/login", UserAccount("bad-email", "does not matter"), String::class.java)
 
         assertTrue(failedResponse.statusCode.is4xxClientError, "The login request should fail")
         assertEquals(HttpStatus.BAD_REQUEST, failedResponse.statusCode)
@@ -58,7 +59,8 @@ class UserLoginTests {
 
     @Test
     fun `Attempting to login with a bad password should fail`() {
-        val failedResponse = restTemplate.postForEntity("/login", UserAccount(userEmail, "bad password"), String::class.java)
+        val failedResponse =
+            restTemplate.postForEntity("/login", UserAccount(userEmail, "bad password"), String::class.java)
 
         assertTrue(failedResponse.statusCode.is4xxClientError, "The login request should fail")
         assertEquals(HttpStatus.UNAUTHORIZED, failedResponse.statusCode)
@@ -66,7 +68,8 @@ class UserLoginTests {
 
     @Test
     fun `Using the right credentials should login successfully`() {
-        val successfulResponse = restTemplate.postForEntity("/login", UserAccount(userEmail, userPassword), String::class.java)
+        val successfulResponse =
+            restTemplate.postForEntity("/login", UserAccount(userEmail, userPassword), String::class.java)
 
         assertTrue(successfulResponse.statusCode.is2xxSuccessful, "The login request should succeed")
         assertEquals(HttpStatus.OK, successfulResponse.statusCode)
@@ -79,14 +82,16 @@ class UserLoginTests {
 
     @Test
     fun `Users can be retrieved with their JWT tokens`() {
-        val successfulResponse = restTemplate.postForEntity("/login", UserAccount(userEmail, userPassword), String::class.java)
+        val successfulResponse =
+            restTemplate.postForEntity("/login", UserAccount(userEmail, userPassword), String::class.java)
 
         assertTrue(successfulResponse.statusCode.is2xxSuccessful, "The login request should succeed")
         assertEquals(HttpStatus.OK, successfulResponse.statusCode)
 
         val token: String = Klaxon().parse<TokenResource>(successfulResponse.body!!)?.token ?: ""
 
-        val userLookupResponse = restTemplate.postForEntity("/authenticate", TokenResource(token), UserAccountResource::class.java)
+        val userLookupResponse =
+            restTemplate.postForEntity("/authenticate", TokenResource(token), UserAccountResource::class.java)
 
         assertTrue(userLookupResponse.statusCode.is2xxSuccessful, "The lookup request should succeed")
         assertEquals(HttpStatus.OK, userLookupResponse.statusCode)
@@ -98,7 +103,8 @@ class UserLoginTests {
 
     @Test
     fun `Users can be retrieved with their JWT token in a header`() {
-        val successfulResponse = restTemplate.postForEntity("/login", UserAccount(userEmail, userPassword), String::class.java)
+        val successfulResponse =
+            restTemplate.postForEntity("/login", UserAccount(userEmail, userPassword), String::class.java)
 
         assertTrue(successfulResponse.statusCode.is2xxSuccessful, "The login request should succeed")
         assertEquals(HttpStatus.OK, successfulResponse.statusCode)
@@ -110,7 +116,8 @@ class UserLoginTests {
 
         val entity = HttpEntity("", headers)
 
-        val userLookupResponse = restTemplate.exchange("/authenticate", HttpMethod.GET, entity, UserAccountResource::class.java)
+        val userLookupResponse =
+            restTemplate.exchange("/authenticate", HttpMethod.GET, entity, UserAccountResource::class.java)
 
         assertTrue(userLookupResponse.statusCode.is2xxSuccessful, "The lookup request should succeed")
         assertEquals(HttpStatus.OK, userLookupResponse.statusCode)
