0df31fa5dc39cbc5561c99836890477844aed2a8
maly7 <paul.aly@outlook.com>
2018-06-05 20:23:16 -0400

    #54 remove token issuer dep

--- authentication-service/src/main/kotlin/io/echoseven/kryption/service/UserAccountService.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/service/UserAccountService.kt
@@ -2,15 +2,13 @@ package io.echoseven.kryption.service
 
 import io.echoseven.kryption.data.UserAccountRepository
 import io.echoseven.kryption.domain.UserAccount
-import io.echoseven.kryption.tokens.TokenIssuer
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserAccountService(
     private val userAccountRepository: UserAccountRepository,
-    private val passwordEncoder: PasswordEncoder,
-    private val tokenIssuer: TokenIssuer
+    private val passwordEncoder: PasswordEncoder
 ) {
 
     fun create(user: UserAccount): UserAccount {
