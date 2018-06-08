c7be52c42ec56630b1a15e19503cf0ce3e4ba5ef
maly7 <paul.aly@outlook.com>
2018-06-07 21:47:33 -0400

    #51 stubbing out functional tests for realtime chat

---
+++ functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/RealtimeChatTests.kt
@@ -0,0 +1,18 @@
+package io.echoseven.kryption.functional
+
+import org.junit.Test
+
+class RealtimeChatTests {
+
+    @Test
+    fun `A User should be able to subscribe to their queue`() {
+    }
+
+    @Test
+    fun `A user cannot subscribe to another user queue`() {
+    }
+
+    @Test
+    fun `A User should receive updates to conversations via their queue`() {
+    }
+}
