00c0d4cd9c7847aac34c965aef57b5beaae51e35
maly7 <paul.aly@outlook.com>
2018-06-07 21:28:18 -0400

    #51 extract out amqp auth service

--- authentication-service/src/main/kotlin/io/echoseven/kryption/web/AmqpAuthenticationController.kt
+++ authentication-service/src/main/kotlin/io/echoseven/kryption/web/AmqpAuthenticationController.kt
@@ -1,14 +1,10 @@
 package io.echoseven.kryption.web
 
-import io.echoseven.kryption.domain.UserAccount
-import io.echoseven.kryption.service.AuthenticationService
-import io.echoseven.kryption.service.UserAccountService
-import io.echoseven.kryption.web.resource.amqp.ALLOW_ACCESS
+import io.echoseven.kryption.service.AmqpAuthenticationService
 import io.echoseven.kryption.web.resource.amqp.DENY_ACCESS
 import io.echoseven.kryption.web.resource.amqp.ResourceCheck
 import io.echoseven.kryption.web.resource.amqp.TopicCheck
 import io.echoseven.kryption.web.resource.amqp.VirtualHostCheck
-import io.jsonwebtoken.MalformedJwtException
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,89 +12,40 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.Optional
 
 @RestController
 @RequestMapping(path = ["/amqp"])
-class AmqpAuthenticationController(
-    val authenticationService: AuthenticationService,
-    val userAccountService: UserAccountService
-) {
+class AmqpAuthenticationController(val amqpAuthenticationService: AmqpAuthenticationService) {
     private val log = LoggerFactory.getLogger(AmqpAuthenticationController::class.java)
 
-    private val supportedResources = listOf("queue")
-
     @PostMapping(path = ["/user"], consumes = [MediaType.ALL_VALUE])
-    fun user(@RequestParam username: String, @RequestParam password: String): String {
-        log.debug("Attempting to authenticate user [{}] to amqp", username)
-
-        val userOpt: Optional<UserAccount>
-
-        try {
-            userOpt = authenticationService.authenticate(password)
-        } catch (e: MalformedJwtException) {
-            return DENY_ACCESS
-        }
-
-        if (userOpt.isPresent && userOpt.get().email == username) {
-            log.debug("Successfully authenticated [{}], granting amqp access", username)
-            return ALLOW_ACCESS
-        }
-
-        log.warn("No user found for [{}], denying access", username)
-        return DENY_ACCESS
-    }
+    fun user(@RequestParam username: String, @RequestParam password: String) =
+        amqpAuthenticationService.user(username, password)
 
     @PostMapping(path = ["/vhost"], consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun vhost(@RequestParam check: VirtualHostCheck) = authVhost(check)
+    fun vhost(@RequestParam check: VirtualHostCheck) =
+        amqpAuthenticationService.vhost(check)
 
     @PostMapping(path = ["/vhost"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     fun vhost(@RequestParam username: String, @RequestParam vhost: String) =
-        authVhost(VirtualHostCheck(username, vhost))
+        amqpAuthenticationService.vhost(VirtualHostCheck(username, vhost))
 
     @PostMapping(path = ["/resource"], consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun resource(@RequestBody check: ResourceCheck) = authResource(check)
+    fun resource(@RequestBody check: ResourceCheck) =
+        amqpAuthenticationService.resource(check)
 
     @PostMapping(path = ["/resource"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
-    fun resource(@RequestParam resource: String, @RequestParam name: String, @RequestParam permission: String, @RequestParam username: String, @RequestParam vhost: String) =
-        authResource(ResourceCheck(resource, name, permission, username, vhost))
+    fun resource(
+        @RequestParam resource: String,
+        @RequestParam name: String,
+        @RequestParam permission: String,
+        @RequestParam username: String,
+        @RequestParam vhost: String
+    ) = amqpAuthenticationService.resource(ResourceCheck(resource, name, permission, username, vhost))
 
     @PostMapping(path = ["/topic"], consumes = [MediaType.ALL_VALUE])
     fun topic(@RequestBody check: TopicCheck): String {
         log.warn("Denying topic access for [{}], this is unsupported", check)
         return DENY_ACCESS
-    }
-
-    private fun authResource(check: ResourceCheck): String {
-        log.debug("Checking access to resource [{}]", check)
-
-        if (!supportedResources.contains(check.resource)) {
-            log.warn(
-                "Denying access to resource [{}], due to unsupported resource type. Current resource types are [{}]",
-                check,
-                supportedResources
-            )
-            return DENY_ACCESS
-        }
-
-        val userOpt = userAccountService.getByEmail(check.username)
-
-        if (userOpt.isPresent && check.name.contains(userOpt.get().id.toString(), true)) {
-            log.debug("Granting access to resource [{}]", check)
-            return ALLOW_ACCESS
-        }
-
-        log.warn("Denying resource access for [{}], either user could not be found or it wasn't their queue", check)
-        return DENY_ACCESS
-    }
-
-    private fun authVhost(check: VirtualHostCheck): String {
-        if (check.username.isEmpty() || check.vhost.isEmpty()) {
-            log.warn("Denying access to vhost [{}] for empty username or vhost field", check)
-            return DENY_ACCESS
-        }
-
-        log.debug("Allowing access to vhost [{}] request", check)
-        return ALLOW_ACCESS
     }
 }
