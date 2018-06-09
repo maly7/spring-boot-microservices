1c9826cf9006625f085c3fe606d89cdc878ce94a
maly7 <paul.aly@outlook.com>
2018-06-09 16:54:37 -0400

    #51 test cleanup

--- functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/support/WebsocketSupport.kt
+++ functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/support/WebsocketSupport.kt
@@ -1,5 +1,29 @@
 package io.echoseven.kryption.functional.support
 
+import org.springframework.http.HttpHeaders
+import org.springframework.messaging.simp.stomp.StompHeaders
+import org.springframework.messaging.simp.stomp.StompSession
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter
+import org.springframework.web.socket.WebSocketHttpHeaders
+import org.springframework.web.socket.messaging.WebSocketStompClient
+
 const val WEBSOCKET_URI = "ws://localhost:8080/realtime/chat"
 
+val adapter = object : StompSessionHandlerAdapter() {}
+
 fun queueName(userId: String) = "/queue/user.$userId"
+
+fun buildStompHeaders(username: String, authToken: String, userId: String): StompHeaders {
+    val stompHeaders = StompHeaders()
+    stompHeaders[StompHeaders.LOGIN] = username
+    stompHeaders[StompHeaders.PASSCODE] = authToken
+    stompHeaders[StompHeaders.DESTINATION] = queueName(userId)
+    return stompHeaders
+}
+
+fun WebSocketStompClient.subscribeToUserQueue(stompHeaders: StompHeaders): StompSession {
+    val webSocketHttpHeaders = WebSocketHttpHeaders()
+    webSocketHttpHeaders[HttpHeaders.UPGRADE] = "websocket"
+
+    return this.connect(WEBSOCKET_URI, webSocketHttpHeaders, stompHeaders, adapter).get()
+}
