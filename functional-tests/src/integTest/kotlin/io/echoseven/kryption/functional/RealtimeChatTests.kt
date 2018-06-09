3a7a48d573bfc2c001f92a797117c4dfc2939e24
maly7 <paul.aly@outlook.com>
2018-06-09 19:08:40 -0400

    #51 tests that actually do something

--- functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/RealtimeChatTests.kt
+++ functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/RealtimeChatTests.kt
@@ -1,12 +1,15 @@
 package io.echoseven.kryption.functional
 
-import io.echoseven.kryption.functional.support.adapter
+import io.echoseven.kryption.functional.support.TestingStompSessionHandlerAdapter
 import io.echoseven.kryption.functional.support.buildStompHeaders
+import io.echoseven.kryption.functional.support.connect
+import io.echoseven.kryption.functional.support.createContactForUser
 import io.echoseven.kryption.functional.support.email
 import io.echoseven.kryption.functional.support.getUserId
+import io.echoseven.kryption.functional.support.login
 import io.echoseven.kryption.functional.support.loginNewUser
 import io.echoseven.kryption.functional.support.password
-import io.echoseven.kryption.functional.support.subscribeToUserQueue
+import io.echoseven.kryption.functional.support.sendMessage
 import org.junit.Before
 import org.junit.Test
 import org.springframework.messaging.converter.MappingJackson2MessageConverter
@@ -15,16 +18,22 @@ import org.springframework.web.socket.messaging.WebSocketStompClient
 import kotlin.test.assertTrue
 
 class RealtimeChatTests {
-    // check out: http://rafaelhz.github.io/testing-websockets/
-    lateinit var stompClient: WebSocketStompClient
-    lateinit var userToken: String
-    lateinit var userId: String
+    private lateinit var stompClient: WebSocketStompClient
+    private lateinit var userToken: String
+    private lateinit var contactToken: String
+    private lateinit var userId: String
+    private lateinit var contactId: String
     private val username = email()
+    private val contact = email()
 
     @Before
     fun setup() {
         userToken = loginNewUser(username, password())
         userId = getUserId(userToken)
+
+        val contactPassword = password()
+        contactId = createContactForUser(userToken, contact, contactPassword)
+        contactToken = login(contact, contactPassword)
 
         stompClient = WebSocketStompClient(StandardWebSocketClient())
         stompClient.messageConverter = MappingJackson2MessageConverter()
@@ -33,15 +42,45 @@ class RealtimeChatTests {
     @Test
     fun `A User should be able to subscribe to their queue`() {
         val stompHeaders = buildStompHeaders(username, userToken, userId)
-        val session = stompClient.subscribeToUserQueue(stompHeaders)
+        val adapter = TestingStompSessionHandlerAdapter()
+        val session = stompClient.connect(stompHeaders, adapter)
 
         session.subscribe(stompHeaders, adapter)
 
         assertTrue(session.isConnected, "The user session should be connected")
+
+        for (i in 1..5) {
+            sendMessage(userToken, contactId, "Test Message $i")
+        }
+
+        for (i in 1..5) {
+            sendMessage(contactToken, userId, "Reply Message $i")
+        }
+
+        Thread.sleep(5000)
+
+        assertTrue(adapter.messages.isNotEmpty())
     }
 
     @Test
     fun `A user cannot subscribe to another user queue`() {
+        val stompHeaders = buildStompHeaders(contact, contactToken, userId)
+        val adapter = TestingStompSessionHandlerAdapter()
+        val session = stompClient.connect(stompHeaders, adapter)
+
+        session.subscribe(stompHeaders, adapter)
+
+        for (i in 1..5) {
+            sendMessage(userToken, contactId, "Test Message $i")
+        }
+
+        for (i in 1..5) {
+            sendMessage(contactToken, userId, "Reply Message $i")
+        }
+
+        Thread.sleep(5000)
+
+        assertTrue(adapter.messages.isEmpty())
     }
 
     @Test
