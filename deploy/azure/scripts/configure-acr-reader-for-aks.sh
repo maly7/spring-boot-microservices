cbd8e77689878859178995148574867299bb1242
maly7 <paul.aly@outlook.com>
2018-07-01 18:47:36 -0400

    add some handy scripts

---
+++ deploy/azure/scripts/configure-acr-reader-for-aks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AKS_RESOURCE_GROUP=kryption
+AKS_CLUSTER_NAME=kryptionProtoCluster
+ACR_RESOURCE_GROUP=kryption
+ACR_NAME=kryptioneastus
+
+# Get the id of the service principal configured for AKS
+CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --query "servicePrincipalProfile.clientId" --output tsv)
+
+# Get the ACR registry resource id
+ACR_ID=$(az acr show --name $ACR_NAME --resource-group $ACR_RESOURCE_GROUP --query "id" --output tsv)
+
+# Create role assignment
+az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
