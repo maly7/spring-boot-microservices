f3bb02b2d3b76bf03a76b7656a091d73cad03807
maly7 <paul.aly@outlook.com>
2018-07-02 23:05:52 -0400

    #64 enable mongo auditing

--- chat-service/src/main/kotlin/io/echoseven/kryption/ChatApp.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/ChatApp.kt
@@ -5,11 +5,13 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableMongoAuditing
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 class ChatApp
