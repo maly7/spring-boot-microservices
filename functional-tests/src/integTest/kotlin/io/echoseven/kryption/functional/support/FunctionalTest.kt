9fa58be73e6f960fb4e2b936480739524680d68f
maly7 <paul.aly@outlook.com>
2018-07-05 21:32:51 -0400

    remove unused var

--- functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/support/FunctionalTest.kt
+++ functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/support/FunctionalTest.kt
@@ -2,16 +2,13 @@ package io.echoseven.kryption.functional.support
 
 import io.restassured.RestAssured
 import org.junit.Before
-import java.net.URI
 
 open class FunctionalTest {
 
     private val baseUri = System.getProperty("funcTestUri", "http://localhost:8080")
-    lateinit var host: String
 
     @Before
     open fun setup() {
-        host = URI(baseUri).host
         RestAssured.baseURI = baseUri
     }
 }
