3e9d6aafcdd2895c810b9f217bca33453993c017
maly7 <paul.aly@outlook.com>
2018-07-05 21:19:07 -0400

    #62 allow for realtime tests to hit remote server

--- functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/support/FunctionalTest.kt
+++ functional-tests/src/integTest/kotlin/io/echoseven/kryption/functional/support/FunctionalTest.kt
@@ -2,13 +2,16 @@ package io.echoseven.kryption.functional.support
 
 import io.restassured.RestAssured
 import org.junit.Before
+import java.net.URI
 
 open class FunctionalTest {
 
     private val baseUri = System.getProperty("funcTestUri", "http://localhost:8080")
+    lateinit var host: String
 
     @Before
     open fun setup() {
+        host = URI(baseUri).host
         RestAssured.baseURI = baseUri
     }
 }
