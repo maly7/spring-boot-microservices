90d4a9e194ca0fec8d4d262e729d7ef82f4e0c72
maly7 <paul.aly@outlook.com>
2018-07-23 22:31:47 -0400

    working on docker files for services

---
+++ api-gateway/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+keytool -import -trustcacerts -file /trusts/tls.crt -keystore trusts.jks -storepass $TRUSTSTORE_PASS -alias "Kryption Root CA" -noprompt
+openssl pkcs12 -export -in /certs/tls.crt -inkey /certs/tls.key -CAfile /trusts/tls.crt -name "api-gateway" -passout pass:$KEYSTORE_PASS -out server.p12
+java -Xmx2000m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=cloud -jar /appapi-gateway-0.1.jar
