5e0358e3085eea5c6ce6e4f1989aec621dea22d5
maly7 <paul.aly@outlook.com>
2018-07-24 19:05:44 -0400

    truststore and keystore creation appears to work

--- api-gateway/entrypoint.sh
+++ api-gateway/entrypoint.sh
@@ -2,4 +2,4 @@
 
 keytool -import -trustcacerts -file /trusts/tls.crt -keystore trusts.jks -storepass $TRUSTSTORE_PASS -alias "Kryption Root CA" -noprompt
 openssl pkcs12 -export -in /certs/tls.crt -inkey /certs/tls.key -CAfile /trusts/tls.crt -name "api-gateway" -passout pass:$KEYSTORE_PASS -out server.p12
-java -Xmx2000m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=cloud -jar /appapi-gateway-0.1.jar
+java -Xmx2000m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=cloud -jar /app/api-gateway-0.1.2.jar
