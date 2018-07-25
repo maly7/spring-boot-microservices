f5f7a49fcccf5737d6a8f64c12c6c8d260ac1857
maly7 <paul.aly@outlook.com>
2018-07-25 19:27:49 -0400

    service registry can register successfully

--- service-registry/entrypoint.sh
+++ service-registry/entrypoint.sh
@@ -2,4 +2,4 @@
 
 keytool -import -trustcacerts -file /trusts/tls.crt -keystore trusts.jks -storepass $TRUSTSTORE_PASS -alias "Kryption Root CA" -noprompt
 openssl pkcs12 -export -in /certs/tls.crt -inkey /certs/tls.key -CAfile /trusts/tls.crt -name "service-registry" -passout pass:$KEYSTORE_PASS -out server.p12
-java -Xmx2000m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=cloud -jar /app/service-registry-0.1.11.jar
+java -Xmx2000m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=cloud  -Djavax.net.ssl.trustStore=/app/trusts.jks -Djavax.net.ssl.trustStorePassword=$TRUSTSTORE_PASS -jar /app/service-registry-0.1.11.jar
