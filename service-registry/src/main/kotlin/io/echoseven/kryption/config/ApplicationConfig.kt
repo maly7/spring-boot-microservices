8f8882f4571723be413ed35fba0d39fec9e2ee97
maly7 <paul.aly@outlook.com>
2018-07-25 19:31:58 -0400

    get rid of the unnecessary

--- service-registry/src/main/kotlin/io/echoseven/kryption/config/ApplicationConfig.kt
+++ service-registry/src/main/kotlin/io/echoseven/kryption/config/ApplicationConfig.kt
@@ -2,11 +2,8 @@ package io.echoseven.kryption.config
 
 import com.netflix.discovery.DiscoveryClient
 import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy
-import org.apache.http.ssl.SSLContextBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.io.File
 
 @Configuration
 class ApplicationConfig {
@@ -14,10 +11,6 @@ class ApplicationConfig {
     @Bean
     fun discoveryClientOptionalArgs(): DiscoveryClient.DiscoveryClientOptionalArgs {
         val args = DiscoveryClient.DiscoveryClientOptionalArgs()
-        args.setSSLContext(
-            SSLContextBuilder.create()
-                .loadTrustMaterial(File("/app/trusts.jks"), "secret".toCharArray(), TrustSelfSignedStrategy()).build()
-        )
         args.setEurekaJerseyClient(
             EurekaJerseyClientImpl.EurekaJerseyClientBuilder()
                 .withSystemSSLConfiguration()
