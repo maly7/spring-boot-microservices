f99c2f27f6e5d5dc9806ac6ec2cf00967174465c
maly7 <paul.aly@outlook.com>
2018-07-24 22:01:22 -0400

    attempting to register over https

---
+++ service-registry/src/main/kotlin/io/echoseven/kryption/config/ApplicationConfig.kt
@@ -0,0 +1,31 @@
+package io.echoseven.kryption.config
+
+import com.netflix.discovery.DiscoveryClient
+import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy
+import org.apache.http.ssl.SSLContextBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.io.File
+
+@Configuration
+class ApplicationConfig {
+
+    @Bean
+    fun discoveryClientOptionalArgs(): DiscoveryClient.DiscoveryClientOptionalArgs {
+        val args = DiscoveryClient.DiscoveryClientOptionalArgs()
+        args.setSSLContext(
+            SSLContextBuilder.create()
+                .loadTrustMaterial(File("/app/trusts.jks"), "secret".toCharArray(), TrustSelfSignedStrategy()).build()
+        )
+        args.setEurekaJerseyClient(
+            EurekaJerseyClientImpl.EurekaJerseyClientBuilder().withSystemSSLConfiguration()
+                .withClientName("registry-client")
+                .withMaxConnectionsPerHost(10)
+                .withMaxTotalConnections(10)
+                .build()
+        )
+
+        return args
+    }
+}
