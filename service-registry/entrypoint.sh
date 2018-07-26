b69a929750b480f7bac0fe64fbc9cd73dae3fa28
maly7 <paul.aly@outlook.com>
2018-07-25 20:20:37 -0400

    rename env variables

--- service-registry/entrypoint.sh
+++ service-registry/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-keytool -import -trustcacerts -file /trusts/tls.crt -keystore trusts.jks -storepass $TRUSTSTORE_PASS -alias "Kryption Root CA" -noprompt
-openssl pkcs12 -export -in /certs/tls.crt -inkey /certs/tls.key -CAfile /trusts/tls.crt -name "service-registry" -passout pass:$KEYSTORE_PASS -out server.p12
-java -Xmx2000m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=cloud  -Djavax.net.ssl.trustStore=/app/trusts.jks -Djavax.net.ssl.trustStorePassword=$TRUSTSTORE_PASS -jar /app/service-registry-0.1.11.jar
+keytool -import -trustcacerts -file /trusts/tls.crt -keystore trusts.jks -storepass $TRUSTSTORE_PASSWORD -alias "Kryption Root CA" -noprompt
+openssl pkcs12 -export -in /certs/tls.crt -inkey /certs/tls.key -CAfile /trusts/tls.crt -name "service-registry" -passout pass:$KEYSTORE_PASSWORD -out server.p12
+java -Xmx2000m -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=cloud  -Djavax.net.ssl.trustStore=/app/trusts.jks -Djavax.net.ssl.trustStorePassword=$TRUSTSTORE_PASSWORD -jar /app/service-registry-0.1.11.jar
