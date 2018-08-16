68f251e49946c2eaacad4c37f6c7218ed8683ece
maly7 <paul.aly@outlook.com>
2018-08-16 19:28:15 -0400

    chat-service starts up under ssl with rabbitmq and mongodb

--- chat-service/src/main/kotlin/io/echoseven/kryption/config/AuthenticatingMongoConfiguration.kt
+++ chat-service/src/main/kotlin/io/echoseven/kryption/config/AuthenticatingMongoConfiguration.kt
@@ -9,7 +9,9 @@ import org.springframework.boot.autoconfigure.mongo.MongoProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
+import javax.net.ssl.SSLContext
 
 @Configuration
 @Profile("!integration")
@@ -18,11 +20,27 @@ class AuthenticatingMongoConfiguration : AbstractMongoConfiguration() {
     @Autowired
     lateinit var mongoProperties: MongoProperties
 
+    @Autowired
+    lateinit var env: Environment
+
     @Bean
     override fun mongoClient(): MongoClient {
-        return MongoClient(listOf(ServerAddress(mongoProperties.host, mongoProperties.port)),
-                MongoCredential.createCredential(mongoProperties.username, mongoProperties.authenticationDatabase, mongoProperties.password),
-                MongoClientOptions.builder().build())
+        // TODO: find a better way of doing this
+        System.setProperty("javax.net.ssl.trustStorePassword", env.getProperty("TRUSTSTORE_PASSWORD"))
+        System.setProperty("javax.net.ssl.keyStorePassword", env.getProperty("KEYSTORE_PASSWORD"))
+
+        return MongoClient(
+            listOf(ServerAddress(mongoProperties.host, mongoProperties.port)),
+            MongoCredential.createCredential(
+                mongoProperties.username,
+                mongoProperties.authenticationDatabase,
+                mongoProperties.password
+            ),
+            MongoClientOptions.builder()
+                .sslEnabled(true)
+                .sslContext(SSLContext.getDefault())
+                .build()
+        )
     }
 
     @Bean
