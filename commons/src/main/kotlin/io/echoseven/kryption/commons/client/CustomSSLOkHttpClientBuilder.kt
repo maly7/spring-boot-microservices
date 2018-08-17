62164b7d9b549f3fda13787ad6a653956d381223
maly7 <paul.aly@outlook.com>
2018-08-16 20:17:00 -0400

    use constructor param

--- commons/src/main/kotlin/io/echoseven/kryption/commons/client/CustomSSLOkHttpClientBuilder.kt
+++ commons/src/main/kotlin/io/echoseven/kryption/commons/client/CustomSSLOkHttpClientBuilder.kt
@@ -4,13 +4,13 @@ import okhttp3.OkHttpClient
 import org.springframework.cloud.commons.httpclient.DefaultOkHttpClientFactory
 import javax.net.ssl.SSLContext
 
-class CustomSSLOkHttpClientFactory(builder: okhttp3.OkHttpClient.Builder?) : DefaultOkHttpClientFactory(builder) {
+class CustomSSLOkHttpClientFactory(val builder: okhttp3.OkHttpClient.Builder) : DefaultOkHttpClientFactory(builder) {
     override fun createBuilder(disableSslValidation: Boolean): OkHttpClient.Builder {
         if (disableSslValidation) {
             return super.createBuilder(disableSslValidation)
         }
 
         //TODO: remove usage of deprecated method
-        return OkHttpClient.Builder().sslSocketFactory(SSLContext.getDefault().socketFactory)
+        return builder.sslSocketFactory(SSLContext.getDefault().socketFactory)
     }
 }
