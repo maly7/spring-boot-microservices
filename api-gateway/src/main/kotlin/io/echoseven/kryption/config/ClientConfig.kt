7a0e404d760b5726da6f2f338eccc2958507dea6
maly7 <paul.aly@outlook.com>
2018-08-17 21:19:43 -0400

    configuring httpclientfactory

--- api-gateway/src/main/kotlin/io/echoseven/kryption/config/ClientConfig.kt
+++ api-gateway/src/main/kotlin/io/echoseven/kryption/config/ClientConfig.kt
@@ -3,10 +3,14 @@ package io.echoseven.kryption.config
 import com.netflix.discovery.DiscoveryClient
 import feign.codec.ErrorDecoder
 import io.echoseven.kryption.clients.error.ClientErrorDecoder
+import io.echoseven.kryption.commons.client.CustomSSLOkHttpClientFactory
 import io.echoseven.kryption.commons.discovery.SSLConfiguredDiscoveryClientOptionalArgs
+import okhttp3.OkHttpClient
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.cloud.commons.httpclient.OkHttpClientFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.web.client.RestTemplate
 
 @Configuration
@@ -22,4 +26,8 @@ class ClientConfig {
     @Bean
     fun discoveryClientOptionalArgs(): DiscoveryClient.DiscoveryClientOptionalArgs =
         SSLConfiguredDiscoveryClientOptionalArgs()
+
+    @Bean
+    fun okHttpClientFactory(env: Environment, builder: OkHttpClient.Builder): OkHttpClientFactory =
+        CustomSSLOkHttpClientFactory(builder, env.getProperty("TRUSTSTORE_PASSWORD")!!)
 }
