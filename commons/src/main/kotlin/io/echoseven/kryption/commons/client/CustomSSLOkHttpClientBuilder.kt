843340b9f6dfaf29e3dc9d9889161be792fdfa9a
maly7 <paul.aly@outlook.com>
2018-08-17 20:59:33 -0400

    conditionally set sys props

--- commons/src/main/kotlin/io/echoseven/kryption/commons/client/CustomSSLOkHttpClientBuilder.kt
+++ commons/src/main/kotlin/io/echoseven/kryption/commons/client/CustomSSLOkHttpClientBuilder.kt
@@ -4,10 +4,22 @@ import okhttp3.OkHttpClient
 import org.springframework.cloud.commons.httpclient.DefaultOkHttpClientFactory
 import javax.net.ssl.SSLContext
 
-class CustomSSLOkHttpClientFactory(val builder: okhttp3.OkHttpClient.Builder) : DefaultOkHttpClientFactory(builder) {
+class CustomSSLOkHttpClientFactory(
+    val builder: okhttp3.OkHttpClient.Builder,
+    private val trustStoreLocation: String,
+    private val trustStorePassword: String
+) : DefaultOkHttpClientFactory(builder) {
     override fun createBuilder(disableSslValidation: Boolean): OkHttpClient.Builder {
         if (disableSslValidation) {
             return super.createBuilder(disableSslValidation)
+        }
+
+        if (System.getProperty("javax.net.ssl.trustStore").isBlank()) {
+            System.setProperty("javax.net.ssl.trustStore", trustStoreLocation)
+        }
+
+        if (System.getProperty("javax.net.ssl.trustStorePassword").isBlank()) {
+            System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword)
         }
 
         //TODO: remove usage of deprecated method
