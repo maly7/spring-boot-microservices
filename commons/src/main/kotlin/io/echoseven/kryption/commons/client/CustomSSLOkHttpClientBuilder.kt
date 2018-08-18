ded639533b3b64293110a72fcb95ef04d64f8e20
maly7 <paul.aly@outlook.com>
2018-08-17 21:41:35 -0400

    attempt to use env variable interpolation

--- commons/src/main/kotlin/io/echoseven/kryption/commons/client/CustomSSLOkHttpClientBuilder.kt
+++ commons/src/main/kotlin/io/echoseven/kryption/commons/client/CustomSSLOkHttpClientBuilder.kt
@@ -5,16 +5,11 @@ import org.springframework.cloud.commons.httpclient.DefaultOkHttpClientFactory
 import javax.net.ssl.SSLContext
 
 class CustomSSLOkHttpClientFactory(
-    val builder: okhttp3.OkHttpClient.Builder,
-    private val trustStorePassword: String
+    val builder: okhttp3.OkHttpClient.Builder
 ) : DefaultOkHttpClientFactory(builder) {
     override fun createBuilder(disableSslValidation: Boolean): OkHttpClient.Builder {
         if (disableSslValidation) {
             return super.createBuilder(disableSslValidation)
-        }
-
-        if (System.getProperty("javax.net.ssl.trustStorePassword").isBlank()) {
-            System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword)
         }
 
         //TODO: remove usage of deprecated method
