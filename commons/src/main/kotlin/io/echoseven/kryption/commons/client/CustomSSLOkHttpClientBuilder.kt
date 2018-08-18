3fdb6a80c201dfcdc300f9085a30ef749f1a19a2
maly7 <paul.aly@outlook.com>
2018-08-17 21:19:43 -0400

    configuring httpclientfactory

--- commons/src/main/kotlin/io/echoseven/kryption/commons/client/CustomSSLOkHttpClientBuilder.kt
+++ commons/src/main/kotlin/io/echoseven/kryption/commons/client/CustomSSLOkHttpClientBuilder.kt
@@ -6,16 +6,11 @@ import javax.net.ssl.SSLContext
 
 class CustomSSLOkHttpClientFactory(
     val builder: okhttp3.OkHttpClient.Builder,
-    private val trustStoreLocation: String,
     private val trustStorePassword: String
 ) : DefaultOkHttpClientFactory(builder) {
     override fun createBuilder(disableSslValidation: Boolean): OkHttpClient.Builder {
         if (disableSslValidation) {
             return super.createBuilder(disableSslValidation)
-        }
-
-        if (System.getProperty("javax.net.ssl.trustStore").isBlank()) {
-            System.setProperty("javax.net.ssl.trustStore", trustStoreLocation)
         }
 
         if (System.getProperty("javax.net.ssl.trustStorePassword").isBlank()) {
