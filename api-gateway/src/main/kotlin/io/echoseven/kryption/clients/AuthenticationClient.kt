da79a6e8dafe0516049d9593991bde60394ecb0e
maly7 <paul.aly@outlook.com>
2018-08-18 22:50:10 -0400

    user registration works if hard-coded https url

--- api-gateway/src/main/kotlin/io/echoseven/kryption/clients/AuthenticationClient.kt
+++ api-gateway/src/main/kotlin/io/echoseven/kryption/clients/AuthenticationClient.kt
@@ -6,7 +6,7 @@ import io.echoseven.kryption.domain.UserResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PostMapping
 
-@FeignClient(name = "authentication-service")
+@FeignClient(name = "authentication-service", url = "https://authentication-service")
 interface AuthenticationClient {
 
     @PostMapping("/user")
