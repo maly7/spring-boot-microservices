0780f7f949bd9542141f1938cd8279558c450980
maly7 <paul.aly@outlook.com>
2018-09-24 20:02:55 -0400

    kotlin version bump + app version bump

--- authentication-service/entrypoint.sh
+++ authentication-service/entrypoint.sh
@@ -2,4 +2,4 @@
 
 keytool -import -trustcacerts -file /trusts/tls.crt -keystore trusts.jks -storepass $TRUSTSTORE_PASSWORD -alias "Kryption Root CA" -noprompt
 openssl pkcs12 -export -in /certs/tls.crt -inkey /certs/tls.key -CAfile /trusts/tls.crt -name "authentication-service" -passout pass:$KEYSTORE_PASSWORD -out server.p12
-exec java $JAVA_OPTS -Djavax.net.ssl.trustStorePassword=$TRUSTSTORE_PASSWORD -jar /app/authentication-service-0.2.3.jar
+exec java $JAVA_OPTS -Djavax.net.ssl.trustStorePassword=$TRUSTSTORE_PASSWORD -jar /app/authentication-service-0.3.0.jar
